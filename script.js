d377539e205ff0791efc39c08bf9cbb89c4bf5f4
Atith794 <68183452+Atith794@users.noreply.github.com>
2023-09-28 07:00:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 //your code here!
+let infy = document.getElementById("infi-list");
+let nextIdx = 11;
 
+
+function addLi(){
+for(let i = 0; i<10 ; i++)
+{
+	const listitm = document.createElement("li");
+	listitm.textContent = "Item" + nextIdx;
+	infy.appendChild()listitm;
+	nextIdx++;
+}
+}
+	infy.addEventListener("scroll",function()=>{
+	if(infy.scrollTop + infy.clientHeight >= infy.scrollHeight)
+	{
+		addLi();
+	}
+});
+
+addLi();
